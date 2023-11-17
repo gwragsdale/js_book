c3f5c153be406efcbb3d9d84fb2fdd3ff47ad58a
Grant Ragsdale <gragsdale1988@gmail.com>
2023-11-17 15:53:17 -0600

    Functions chapter, main part (exercises to come later)

---
+++ intro/functions/three_ways.js
@@ -0,0 +1,15 @@
+/* greetPeople();
+
+function greetPeople() {
+  console.log('Good Morning!');
+}
+*/
+
+let greetPeople = function() {
+  console.log("Good Morning!");
+}
+
+greetPeople();
+
+let add = (a, b) => a + b;
+console.log(add(1, 1));
