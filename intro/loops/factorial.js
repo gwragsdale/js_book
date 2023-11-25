9a5da7bdecfddb471a4a1541db6c2c3d6175a8b5
Grant Ragsdale <gragsdale1988@gmail.com>
2023-11-25 09:41:49 -0600

    loop chapter from home cpu

---
+++ intro/loops/factorial.js
@@ -0,0 +1,12 @@
+function factorial(number) {
+  let result = 1;
+  for (let counter = number; counter > 0; counter -= 1) {
+    result *= counter;
+  }
+
+  return result;
+}
+
+console.log(factorial(5));
+console.log(factorial(4));
+console.log(factorial(3));
